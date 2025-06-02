0a89f55bc665665dc72ff3aea95cecd70c210894
Reuploads <sigmadarling3@gmail.com>
2025-06-01 22:04:19 -0500

    Update rename files.sh

--- asset archive/rename files.sh
+++ asset archive/rename files.sh
@@ -6,13 +6,19 @@ cd "$base" || exit 1
 logfile="rename_log.json"
 > "$logfile"
 
+echo "[" >> "$logfile"
+
 find . -type f | while read -r filepath; do
     relpath="${filepath#./}"
+
+    # Skip already renamed log file
+    [[ "$relpath" == "$logfile" ]] && continue
+
     mime=$(file -b --mime-encoding "$filepath")
+    ext=""
 
-    first_bytes=$(head -c 128 "$filepath" | tr -d '\0')
-
-    if file "$filepath" | grep -q 'ASCII\|UTF-8'; then
+    if [[ "$mime" == "utf-8" || "$mime" == "us-ascii" ]]; then
+        first_bytes=$(head -c 256 "$filepath" | tr -d '\0')
         if [[ "$first_bytes" == *"<roblox version="* ]]; then
             ext="rbxmx"
         elif [[ "$first_bytes" =~ ^\{ ]]; then
@@ -24,11 +30,19 @@ find . -type f | while read -r filepath; do
         fi
     else
         magic=$(xxd -l 4 -p "$filepath" 2>/dev/null | tr '[:lower:]' '[:upper:]')
+
         case "$magic" in
             4F676753) ext="ogg" ;;
             89504E47) ext="png" ;;
             FFD8FFE0|FFD8FFE1|FFD8FFE2) ext="jpg" ;;
-            *) ext="bin" ;;
+            *)
+                # fallback: check if it *contains* mesh signature
+                if grep -a -m1 -q "^version [0-9]\+\.[0-9]\+" "$filepath"; then
+                    ext="mesh"
+                else
+                    ext="bin"
+                fi
+                ;;
         esac
     fi
 
@@ -40,7 +54,6 @@ find . -type f | while read -r filepath; do
     fi
 done
 
-# Remove trailing comma and wrap in brackets to make valid JSON
+# Finalize JSON
 sed -i '$ s/,$//' "$logfile"
-sed -i '1s/^/[/' "$logfile"
 echo "]" >> "$logfile"
