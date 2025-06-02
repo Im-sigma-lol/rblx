aa706d8a2eddb4f3eb046dd0b2ce618f1e481a72
Reuploads <sigmadarling3@gmail.com>
2025-06-01 21:47:20 -0500

    Update rename files.sh

--- asset archive/rename files.sh
+++ asset archive/rename files.sh
@@ -11,60 +11,50 @@ for file in "$dir"/**; do
   ext="${name##*.}"
 
   if [[ "$encoding" == "utf-8" ]]; then
-    headline=$(head -n 1 "$file")
+    headline=$(head -n 1 "$file" | tr -d '\r\n')
 
     if [[ "$headline" == \{* ]]; then
       newext="json"
     elif grep -q "<roblox" "$file"; then
       newext="rbxmx"
-    elif grep -q "<roblox" "$file" && grep -q "Saved by" "$file"; then
-      newext="rbxmx"
-    elif [[ "$headline" =~ ^version[[:space:]]+[0-9]+ ]]; then
+    elif [[ "$headline" =~ ^version\ [0-9]+\.[0-9]+$ ]]; then
       newext="mesh"
     else
       newext="txt"
     fi
   else
-    sig4=$(dd if="$file" bs=1 count=4 2>/dev/null | hexdump -v -e '/1 "%02X"')
-
-    case "$sig4" in
-      4F676753)
-        newext="ogg"
-        ;;
-      664C6143)
-        newext="flac"
-        ;;
-      52494646)
-        if head -c 512 "$file" | grep -q "WAVE"; then
-          newext="wav"
-        elif head -c 512 "$file" | grep -q "WEBP"; then
-          newext="webp"
-        else
-          newext="bin"
-        fi
-        ;;
-      89504E47)
-        newext="png"
-        ;;
-      47494638)
-        newext="gif"
-        ;;
-      25504446)
-        newext="pdf"
-        ;;
-      494433*)
-        newext="mp3"
-        ;;
-      *)
-        if head -c 512 "$file" | grep -q '#EXTM3U'; then
-          newext="m3u8"
-        elif head -c 512 "$file" | grep -q -E '^TS[a-zA-Z]'; then
-          newext="ts"
-        else
-          newext="bin"
-        fi
-        ;;
-    esac
+    headline=$(head -n 1 "$file" | tr -d '\r\n')
+    if [[ "$headline" =~ ^version\ [0-9]+\.[0-9]+$ ]]; then
+      newext="mesh"
+    else
+      sig4=$(dd if="$file" bs=1 count=4 2>/dev/null | hexdump -v -e '/1 "%02X"')
+      case "$sig4" in
+        4F676753) newext="ogg" ;;
+        664C6143) newext="flac" ;;
+        52494646)
+          if head -c 512 "$file" | grep -q "WAVE"; then
+            newext="wav"
+          elif head -c 512 "$file" | grep -q "WEBP"; then
+            newext="webp"
+          else
+            newext="bin"
+          fi
+          ;;
+        89504E47) newext="png" ;;
+        47494638) newext="gif" ;;
+        25504446) newext="pdf" ;;
+        494433*) newext="mp3" ;;
+        *)
+          if head -c 512 "$file" | grep -q '#EXTM3U'; then
+            newext="m3u8"
+          elif head -c 512 "$file" | grep -q -E '^TS[a-zA-Z]'; then
+            newext="ts"
+          else
+            newext="bin"
+          fi
+          ;;
+      esac
+    fi
   fi
 
   base="${file%.*}"
