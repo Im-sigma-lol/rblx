a68c991e39e0fdbec2128602a9988e0bc26c75c7
Reuploads <sigmadarling3@gmail.com>
2025-06-03 00:11:03 -0500

    Update loader.lua

--- scripts for rblx/universal/f3x/loader.lua
+++ scripts for rblx/universal/f3x/loader.lua
@@ -1,1 +1,13 @@
-raw.githubusercontent.com/Im-sigma-lol/rblx/refs/heads/main/scripts%20for%20rblx/universal/f3x/F3X.rbxm
+local path = "F3X/F3X.rbxm"
+local url = "raw.githubusercontent.com/Im-sigma-lol/rblx/refs/heads/main/scripts%20for%20rblx/universal/f3x/F3X.rbxm"
+
+-- Download if not already saved
+if not isfile(path) then
+    writefile(path, game:HttpGet(url))
+end
+
+-- Load model from file
+local objects = { LoadCustomInstance(path) }
+
+-- Run the first object's Source (just like game:GetObjects()[1].Source)
+loadstring(objects[1].Source)()
